c67fb260bf00b371cd7cb524bddb7e4b488ec5e8
tchibo71 <tchibo71@gmail.com>
2026-01-26 03:40:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,19 +1,3 @@
-import Setup from './pages/Setup';
-import Dashboard from './pages/Dashboard';
-import Study from './pages/Study';
-import Review from './pages/Review';
-import History from './pages/History';
-
-
-export const PAGES = {
-    "Setup": Setup,
-    "Dashboard": Dashboard,
-    "Study": Study,
-    "Review": Review,
-    "History": History,
-}
-
-export const pagesConfig = {
-    mainPage: "Setup",
-    Pages: PAGES,
+export default {
+  mainPage: "Setup"
 };
